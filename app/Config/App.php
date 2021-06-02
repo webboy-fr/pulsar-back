6aac4224b561e235591c485bc2c41bf2f5a0d7b8
fyzalis <julien.buabent@gmail.com>
2021-06-02 21:33:24 +0200

    test

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
 	 *
 	 * @var string
 	 */
-	public $baseURL = 'http://pulsar.local/api';
+	public $baseURL = '';
 
 	/**
 	 * --------------------------------------------------------------------------
--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,20 +6,7 @@ class Home extends BaseController
 {
 	public function index()
 	{
-		//return view('welcome_message');
-
-		echo base_url();
-
 		exit('nothing to see here');
-	}
-
-	public function api()
-	{
-		//return view('welcome_message');
-
-		echo base_url();
-
-		exit('nothing to see here API');
 	}
 
 }
