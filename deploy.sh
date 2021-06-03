22b1167bf5b03896ba2fe2320e471082f104d629
fyzalis <julien.buabent@gmail.com>
2021-06-03 11:25:18 +0200

    test deploiement

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,20 @@
-php spark migrate
+composer create-project codeigniter4/appstarter pulsar-back
+cd pulsar-back
+git init
+git remote add origin https://github.com/webboy-fr/pulsar-back.git
+
+rm -Rf app
+rm -Rf public
+rm env
+
+git fetch origin
+#git reset --hard origin/master
+git stash
+git pull origin master
+
+cp ../.env .
+
+
+#php spark migrate
 #php spark db:seed MemberSeeder
-./vendor/bin/phpunit tests
+#./vendor/bin/phpunit tests
