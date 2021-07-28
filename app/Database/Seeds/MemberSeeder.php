699c7aa7a58f719eb0c8a38786fcf234a0cf8e6c
fyzalis <julien.buabent@gmail.com>
2021-07-28 10:55:51 +0200

    test migration + test

--- app/Database/Seeds/MemberSeeder.php
+++ app/Database/Seeds/MemberSeeder.php
@@ -4,21 +4,15 @@ namespace App\Database\Seeds;
 
 use CodeIgniter\Database\Seeder;
 
-class MemberSeeder extends Seeder
-{
-        public function run()
-        {
-                $model = model('MemberModel');
-
-
-                //static::faker->create('fr_FR');
-                for($i=0; $i<5; $i++){
-                        $model->insert([
-                                'firstname'      => static::faker()->name,
-                                'lastname'      => static::faker()->name                        
-                        ]);
-                }
-
-                
+class MemberSeeder extends Seeder {
+    public function run() {
+        $model = model('MemberModel');
+        for ($i = 0; $i < 5; $i++) {
+            $model->insert([
+                'firstname' => static::faker()->name,
+                'lastname'  => static::faker()->name,
+            ]);
         }
+
+    }
 }
